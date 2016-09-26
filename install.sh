ab1bec16b3487987fbce12874c7233a09f3bac36
Sebastian Caceres <sebastian.caceresb@gmail.com>
2016-09-27 00:04:53 +0200

    Add Archlinux support

--- install.sh
+++ install.sh
@@ -11,11 +11,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [ -d ~/.vim/bundle/Vundle.vim ] || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ## Install The Silver Searcher
-# TODO: Add support for CentOS and ArchLinux
+# TODO: Add support for CentOS
 # OSX
 ! uname -a | grep -i darwin || brew install the_silver_searcher
 # Debian/Ubuntu
 ! uname -a | grep -i ubuntu || sudo apt-get install silversearcher-ag
+# Archlinux
+! uname -a | grep -i arch || sudo pacman -S the_silver_searcher
 
 # Symlink colors and plugin directories
 [ -e ~/.vim/colors ] || ln -s $DIR/colors ~/.vim/colors
