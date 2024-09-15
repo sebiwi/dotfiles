199314f592d63ff44352b50c9ba738ffdcc82ecb
sebiwi <sebastian.caceresb@gmail.com>
2024-09-15 23:34:37 +0200

    add file history mappings

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -12,6 +12,31 @@ return {
         "<cmd>Neogit<cr>",
         desc = "Neogit",
       },
+      {
+        "<leader>gl",
+        function()
+          local file = vim.fn.expand("%")
+          require("neogit").action("log", "log_current", { "--", file })()
+        end,
+        desc = "Neogit Log for this file",
+      },
+      {
+        "<leader>gl",
+        function()
+          local file = vim.fn.expand("%")
+          vim.cmd([[execute "normal! \<ESC>"]])
+          local line_start = vim.fn.getpos("'<")[2]
+          local line_end = vim.fn.getpos("'>")[2]
+          require("neogit").action("log", "log_current", { "-L" .. line_start .. "," .. line_end .. ":" .. file })()
+        end,
+        desc = "Neogit Log for this range",
+        mode = "v",
+      },
+      {
+        "<LEADER>gf",
+        "<cmd>DiffviewFileHistory %<cr>",
+        desc = "Current File History",
+      },
     },
     config = true,
   },
