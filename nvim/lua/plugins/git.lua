391abae183bc8c4f520b28c6897f88ddf1b5cf7f
sebiwi <sebastian.caceresb@gmail.com>
2024-09-15 22:18:16 +0200

    add neogit and surround

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,3 +1,11 @@
 return {
-  { "tpope/vim-fugitive" },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true,
+  },
 }
