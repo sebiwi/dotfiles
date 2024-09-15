67608731a6560675cd8678f02806024e215d8c54
sebiwi <sebastian.caceresb@gmail.com>
2024-09-15 20:40:04 +0200

    still punching lazyvim in the face

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,4 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "windwp/nvimp-spectre", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
---
+++ nvim/lua/plugins/extend-indentscope.lua
@@ -0,0 +1,6 @@
+return {
+  "echasnovski/mini.indentscope",
+  opts = {
+    draw = { animation = require("mini.indentscope").gen_animation.none() },
+  },
+}
