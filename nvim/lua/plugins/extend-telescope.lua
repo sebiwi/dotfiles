6558615fff4831e06ee89a28d28c2d8fbfe09a21
sebiwi <sebastian.caceresb@gmail.com>
2024-09-16 10:17:13 +0200

    add newtab telescope binding

---
+++ nvim/lua/plugins/extend-telescope.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = function(_, opts)
+    local actions = require("telescope.actions")
+    opts.defaults = {
+      mappings = {
+        i = {
+          ["<c-t>"] = actions.select_tab,
+        },
+      },
+    }
+  end,
+}
