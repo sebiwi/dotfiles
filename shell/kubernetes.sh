83442f66e3bafe4131fab4ec42b1fdd08288af52
sebiwi <sebastian.caceresb@gmail.com>
2024-09-20 13:51:07 +0200

    update kubernetes configuration

--- shell/kubernetes.sh
+++ shell/kubernetes.sh
@@ -1,11 +1,4 @@
-# kube-ps1
-kon() {
-   export KUBE_ON=true
-   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-   source ~/stuff/dotfiles/shell/kubernetes_aliases.sh
-   source <(kubectl completion zsh)
-}
-
-koff() {
-    unset KUBE_ON
-}
+autoload -Uz compinit
+compinit
+source <(kubectl completion zsh)
+source ~/stuff/dotfiles/shell/kubernetes_aliases.sh
