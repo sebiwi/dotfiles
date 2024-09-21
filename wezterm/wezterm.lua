84f5a27a9772c2078e689af029cde583b8734496
sebiwi <sebastian.caceresb@gmail.com>
2024-09-21 12:49:16 +0200

    add wez-tmux for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
-local act = wezterm.action
 local config = wezterm.config_builder()
 
 --- plugins
 local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+local wez_tmux = wezterm.plugin.require("https://github.com/sei40kr/wez-tmux")
 
 --- general
 config.audible_bell = "Disabled"
@@ -29,169 +29,7 @@ config.macos_window_background_blur = 10
 config.leader = {
 	key = "a",
 	mods = "CTRL",
-	timeout_milliseconds = 2000,
 }
-
-config.keys = {
-	{
-		mods = "LEADER",
-		key = "[",
-		action = act.ActivateCopyMode,
-	},
-	{
-		mods = "LEADER",
-		key = "z",
-		action = act.TogglePaneZoomState,
-	},
-	{
-		mods = "LEADER",
-		key = "c",
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		mods = "LEADER",
-		key = "p",
-		action = act.ActivateTabRelative(-1),
-	},
-	{
-		mods = "LEADER",
-		key = "n",
-		action = act.ActivateTabRelative(1),
-	},
-	{
-		mods = "LEADER",
-		key = "%",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = '"',
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = ",",
-		action = act.PromptInputLine({
-			description = "Enter new name for tab",
-			action = wezterm.action_callback(function(window, pane, line)
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
-		}),
-	},
-	{
-		mods = "LEADER",
-		key = "h",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		mods = "LEADER",
-		key = "j",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	{
-		mods = "LEADER",
-		key = "k",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		mods = "LEADER",
-		key = "l",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		mods = "LEADER",
-		key = "LeftArrow",
-		action = act.AdjustPaneSize({ "Left", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "RightArrow",
-		action = act.AdjustPaneSize({ "Right", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "DownArrow",
-		action = act.AdjustPaneSize({ "Down", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "UpArrow",
-		action = act.AdjustPaneSize({ "Up", 5 }),
-	},
-}
-
-config.key_tables = {
-	copy_mode = {
-		{ key = "q", mods = "NONE", action = act.CopyMode("Close") },
-
-		{ key = "h", mods = "NONE", action = act.CopyMode("MoveLeft") },
-		{ key = "j", mods = "NONE", action = act.CopyMode("MoveDown") },
-		{ key = "k", mods = "NONE", action = act.CopyMode("MoveUp") },
-		{ key = "l", mods = "NONE", action = act.CopyMode("MoveRight") },
-
-		{ key = "w", mods = "NONE", action = act.CopyMode("MoveForwardWord") },
-		{ key = "e", mods = "NONE", action = act.CopyMode("MoveForwardWordEnd") },
-
-		{ key = "b", mods = "NONE", action = act.CopyMode("MoveBackwardWord") },
-
-		{ key = "0", mods = "NONE", action = act.CopyMode("MoveToStartOfLine") },
-		{ key = "$", mods = "NONE", action = act.CopyMode("MoveToEndOfLineContent") },
-		{ key = "_", mods = "NONE", action = act.CopyMode("MoveToStartOfLineContent") },
-
-		{ key = "v", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Cell" }) },
-		{ key = "V", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Line" }) },
-		{ key = "v", mods = "CTRL", action = act.CopyMode({ SetSelectionMode = "Block" }) },
-
-		{ key = "G", mods = "NONE", action = act.CopyMode("MoveToScrollbackBottom") },
-		{ key = "g", mods = "NONE", action = act.CopyMode("MoveToScrollbackTop") },
-
-		{ key = "H", mods = "NONE", action = act.CopyMode("MoveToViewportTop") },
-		{ key = "M", mods = "NONE", action = act.CopyMode("MoveToViewportMiddle") },
-		{ key = "L", mods = "NONE", action = act.CopyMode("MoveToViewportBottom") },
-
-		{ key = "u", mods = "CTRL", action = act.CopyMode("PageUp") },
-		{ key = "d", mods = "CTRL", action = act.CopyMode("PageDown") },
-
-		-- Enter y to copy and quit the copy mode.
-		{
-			key = "y",
-			mods = "NONE",
-			action = act.Multiple({
-				act.CopyTo("ClipboardAndPrimarySelection"),
-				act.CopyMode("Close"),
-			}),
-		},
-		-- Enter search mode to edit the pattern.
-		-- When the search pattern is an empty string the existing pattern is preserved
-		{ key = "/", mods = "NONE", action = act({ Search = { CaseSensitiveString = "" } }) },
-		{ key = "?", mods = "NONE", action = act({ Search = { CaseInSensitiveString = "" } }) },
-		{ key = "n", mods = "CTRL", action = act({ CopyMode = "NextMatch" }) },
-		{ key = "p", mods = "CTRL", action = act({ CopyMode = "PriorMatch" }) },
-	},
-
-	search_mode = {
-		{ key = "Escape", mods = "NONE", action = act({ CopyMode = "Close" }) },
-		-- Go back to copy mode when pressing enter, so that we can use unmodified keys like "n"
-		-- to navigate search results without conflicting with typing into the search area.
-		{ key = "Enter", mods = "NONE", action = "ActivateCopyMode" },
-		{ key = "c", mods = "CTRL", action = "ActivateCopyMode" },
-		{ key = "n", mods = "CTRL", action = act({ CopyMode = "NextMatch" }) },
-		{ key = "p", mods = "CTRL", action = act({ CopyMode = "PriorMatch" }) },
-		{ key = "r", mods = "CTRL", action = act.CopyMode("CycleMatchType") },
-		{ key = "u", mods = "CTRL", action = act.CopyMode("ClearPattern") },
-	},
-}
-
--- leader + number to activate that tab
-for i = 0, 9 do
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "LEADER",
-		action = act.ActivateTab(i),
-	})
-end
 
 -- smart splits config
 smart_splits.apply_to_config(config, {
@@ -223,5 +61,8 @@ tabline.setup({
 	},
 })
 tabline.apply_to_config(config)
+
+-- wez-tmux config
+wez_tmux.apply_to_config(config)
 
 return config
