e3d0cb044cf1c7a03cb6b339c911681f71cd14fe
sebiwi <sebastian.caceresb@gmail.com>
2024-10-05 19:42:47 +0200

    add toggle for copilot

---
+++ nvim/lua/plugins/extend-copilot.lua
@@ -0,0 +1,16 @@
+return {
+  "zbirenbaum/copilot.lua",
+  keys = {
+    {
+      "<leader>at",
+      function()
+        if require("copilot.client").is_disabled() then
+          require("copilot.command").enable()
+        else
+          require("copilot.command").disable()
+        end
+      end,
+      desc = "Toggle (Copilot)",
+    },
+  },
+}
---
+++ nvim/lua/plugins/extend-which-key.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    icons = {
+      rules = {
+        { plugin = "copilot.lua", icon = " ", color = "orange" },
+      },
+    },
+  },
+}
