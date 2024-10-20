542bf0acfd0ee0cc6c13158e33d0b01e1473f0d8
sebiwi <sebastian.caceresb@gmail.com>
2024-10-20 20:43:22 +0200

    add obsidian configuration

--- nvim/lua/plugins/extend-which-key.lua
+++ nvim/lua/plugins/extend-which-key.lua
@@ -1,6 +1,9 @@
 return {
   "folke/which-key.nvim",
   opts = {
+    spec = {
+      { "<leader>o", group = "obsidian" },
+    },
     icons = {
       rules = {
         { plugin = "copilot.lua", icon = " ", color = "orange" },
---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,132 @@
+return {
+  "epwalsh/obsidian.nvim",
+  lazy = true,
+  version = "*", -- recommended, use latest release instead of latest commit
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    mappings = {
+      --- follow the link under the cursor
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+    workspaces = {
+      {
+        name = "notes",
+        path = "~/stuff/notes",
+      },
+    },
+    -- disable checkboxes and bullets
+    ui = {
+      checkboxes = {},
+      bullets = {},
+    },
+    -- note ID: title ? %Y-%m-%d-title : %Y-%m-%d-Untitled
+    note_id_func = function(title)
+      local suffix = ""
+      if title ~= nil then
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-_]", ""):lower()
+      else
+        suffix = "Untitled"
+      end
+      return tostring(os.date("%Y-%m-%d")) .. "-" .. suffix
+    end,
+    -- Open the URL in the default web browser.
+    ---@param url string
+    follow_url_func = function(url)
+      vim.fn.jobstart({ "open", url }) -- Mac OS
+    end,
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an image
+    -- file it will be ignored but you can customize this behavior here.
+    ------@param img string
+    ---follow_img_func = function(img)
+    ---  vim.fn.jobstart({ "qlmanage", "-p", img }) -- Mac OS quick look preview
+    ---  -- vim.fn.jobstart({"xdg-open", url})  -- linux
+    ---  -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+    ---end,
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+  },
+  keys = {
+    {
+      desc = "Open Note in Obisidian",
+      "<LEADER>oo",
+      "<cmd>ObsidianOpen<cr>",
+    },
+    {
+      desc = "Create New Note",
+      "<LEADER>on",
+      "<cmd>ObsidianNew<cr>",
+    },
+    {
+      desc = "Rename Note",
+      "<LEADER>or",
+      "<cmd>ObsidianRename<cr>",
+    },
+    {
+      desc = "Insert Template",
+      "<LEADER>oi",
+      "<cmd>ObsidianTemplate<cr>",
+    },
+    {
+      desc = "Create Checkbox",
+      "<LEADER>oc",
+      "<cmd>ObsidianToggleCheckbox<cr>",
+    },
+    {
+      desc = "Paste Image from Clipboard",
+      "<LEADER>op",
+      "<cmd>ObsidianPasteImg<cr>",
+    },
+    {
+      desc = "Search Notes",
+      "<LEADER>os",
+      "<cmd>ObsidianQuickSwitch<cr>",
+    },
+    {
+      desc = "Grep Notes",
+      "<LEADER>og",
+      "<cmd>ObsidianSearch<cr>",
+    },
+    {
+      desc = "Search Current Page Links",
+      "<LEADER>ol",
+      "<cmd>ObsidianLinks<cr>",
+    },
+    {
+      desc = "Search Backlinks",
+      "<LEADER>ob",
+      "<cmd>ObsidianBacklinks<cr>",
+    },
+    {
+      desc = "Search Tags",
+      "<LEADER>ot",
+      "<cmd>ObsidianTags<cr>",
+    },
+  },
+}
