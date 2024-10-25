f8431f1a6a09a98bde30d72412629570d0a7a641
sebiwi <sebastian.caceresb@gmail.com>
2024-10-25 19:33:33 +0200

    add mappings for newline and move lines

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Add newline
+vim.keymap.set(
+  "n",
+  "[<space>",
+  "<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>",
+  { desc = "Add newline above" }
+)
+vim.keymap.set(
+  "n",
+  "]<space>",
+  "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>",
+  { desc = "Add newline below" }
+)
+--- move lines
+vim.keymap.set("n", "<S-Down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<S-Up>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("v", "<S-Down>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<S-Up>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
