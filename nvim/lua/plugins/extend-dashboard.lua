efd89f77398f2e027087d331d0ae01cdd7da1912
sebiwi <sebastian.caceresb@gmail.com>
2024-12-06 21:42:54 +0100

    update dashboard

--- nvim/lua/plugins/extend-dashboard.lua
+++ nvim/lua/plugins/extend-dashboard.lua
@@ -2,33 +2,56 @@ return {
   "folke/snacks.nvim",
   opts = {
     dashboard = {
-      --       preset = {
-      --         header = [[
-      -- ██████╗  ██████╗     ██╗████████╗
-      -- ██╔══██╗██╔═══██╗    ██║╚══██╔══╝
-      -- ██║  ██║██║   ██║    ██║   ██║
-      -- ██║  ██║██║   ██║    ██║   ██║
-      -- ██████╔╝╚██████╔╝    ██║   ██║
-      -- ╚═════╝  ╚═════╝     ╚═╝   ╚═╝
-      --
-      --         ]],
-      --       },
       sections = {
-        { section = "header", padding = 1 },
-        { section = "keys", gap = 1, padding = 1 },
+        { section = "header" },
         {
+          pane = 2,
           section = "terminal",
-          icon = " ",
-          title = "Git Status",
-          enabled = function()
-            return Snacks.git.get_root() ~= nil
-          end,
-          cmd = "hub status --short --branch --renames",
+          cmd = "colorscript -e square",
           height = 5,
           padding = 1,
-          indent = 3,
-          ttl = 5,
         },
+        { section = "keys", gap = 1, padding = 1 },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              title = "Notifications",
+              cmd = "gh notify -s -a -n5",
+              icon = " ",
+              height = 5,
+              enabled = true,
+            },
+            {
+              title = "Open Issues",
+              cmd = "gh issue list -L 3",
+              icon = " ",
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Open PRs",
+              cmd = "gh pr list -L 3",
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Git Status",
+              cmd = "hub --no-pager diff --stat -B -M -C",
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend("force", {
+              pane = 2,
+              section = "terminal",
+              enabled = in_git,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
         { section = "startup" },
       },
     },
